6623e9a6bf4abd0c953b56a8de06ff4943aa6189
Isaiah Sherrill <ijshd7@mail.missouri.edu>
2021-02-28 02:18:40 -0600

    working prototype with react and hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,148 +1,211 @@
-import * as React from "react"
+import React, { useState, useEffect } from 'react';
+import he from 'he';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+const mainStyle = {
+  textAlign: 'center',
+  marginLeft: 'auto',
+  marginRight: 'auto',
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+const headerStyle = {
+  color: '#000000',
 }
-const headingAccentStyles = {
-  color: "#663399",
+const paraStyle = {
+  marginTop: '2rem',
 }
-const paragraphStyles = {
-  marginBottom: 48,
+const buttonStyle = {
+  marginTop: '1rem',
+  marginLeft: '2rem',
+  marginRight: '2rem',
 }
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
+const anchorStyle = {
+  bottom: '0',
+  position: 'fixed',
+  marginLeft: '-5rem',
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
-
-// markup
 const IndexPage = () => {
+  const [state, setState] = useState({
+    loading: true,
+    error: false,
+    data: [],
+  });
+  const [page, setPage] = useState(0);
+  const [score, setScore] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
+  const [right, setRight] = useState([]);
+  const [wrong, setWrong] = useState([]);
+  const [visibleResults, setVisibleResults] = useState(false);
+  let updatedScore = 0;
+  let rights = [];
+  let wrongs = [];
+  const calcScore = (x, y) => {
+    for (let i = 0; i < 10; i++) {
+      if (x[i] === y[i]) {
+        updatedScore = updatedScore + 1;
+        rights.push(i);
+      } else wrongs.push(i);
+    }
+  }
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        const correctAnswer = json.results.map(q => q['correct_answer']);
+        const questionBulk = json.results.map(q => q['question']);
+        setState({
+          data: json.results,
+          loading: false,
+          error: false,
+        });
+        setCorrectAnswers(correctAnswers.concat(correctAnswer));
+        setAllQuestions(allQuestions.concat(questionBulk));
+      })
+      .catch(err => {
+        setState({error: err})
+      })
+  }, [])
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+    <main style={mainStyle}>
+      {state.loading && (
+        <h1 style={headerStyle}>LOADING...</h1>
+      )}
+      {page === 0 && (
+        <>
+          <h1 style={headerStyle}>Welcome to the Trivia Challenge!</h1>
+          <p style={paraStyle}>You will be presented with 10 True or False questions.</p>
+          <p style={paraStyle}>Can you score 100%?</p>
+          <button 
+            style={buttonStyle}
+            onClick={() => setPage(page + 1)}
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+            BEGIN
+          </button>
+        </>
+      )} 
+      {(page > 0 && page < 10) && (
+        <>
+          <h1 style={headerStyle}>
+            {state.data[page - 1].category}
+          </h1>
+          <p style={paraStyle}>
+            Difficulty: {state.data[page - 1].difficulty}
+          </p>
+          <p style={paraStyle}>
+            {he.decode(`${state.data[page - 1].question}`)}
+          </p>
+          <p style={paraStyle}>
+            {page} of 10
+          </p>
+          <button
+            style={buttonStyle}
+            onClick={() => {
+              setAnswers(answers.concat('True'));
+              setPage(page + 1)
+            }}
+          >
+            TRUE
+          </button>
+          <button
+            style={buttonStyle}
+            onClick={() => {
+              setAnswers(answers.concat('False'));
+              setPage(page + 1)
+            }}
+          >
+            FALSE
+          </button>
+        </>
+      )}
+      {(page === 10 && (
+        <>
+          <h1 style={headerStyle}>
+            {state.data[page - 1].category}
+          </h1>
+          <p style={paraStyle}>
+            Difficulty: {state.data[page - 1].difficulty}
+          </p>
+          <p style={paraStyle}>
+            {he.decode(`${state.data[page - 1].question}`)}
+          </p>
+          <p style={paraStyle}>
+            {page} of 10
+          </p>
+          <button
+            style={buttonStyle}
+            onClick={() => {
+              setAnswers(answers.concat('True'));
+              setPage(page + 1);
+              calcScore(answers, correctAnswers);
+              setScore(score + updatedScore);
+              setRight(right.concat(rights));
+              setWrong(wrong.concat(wrongs));
+            }}
+          >
+            TRUE
+          </button>
+          <button
+            style={buttonStyle}
+            onClick={() => {
+              setAnswers(answers.concat('False'));
+              setPage(page + 1);
+              calcScore(answers, correctAnswers);
+              setScore(score + updatedScore);
+              setRight(right.concat(rights));
+              setWrong(wrong.concat(wrongs));
+            }}
+          >
+            FALSE
+          </button>
+        </>
+      ))}
+      {(page >= 11 && (
+        <>
+          <h1 style={headerStyle}>
+            You Finished!
+          </h1>
+          <p style={paraStyle}>
+            Your score was {score}/10
+          </p>
+          <button
+            style={buttonStyle}
+            onClick={() => {
+              setVisibleResults(!visibleResults);
+            }}
+          >
+            {!visibleResults && `SHOW RESULTS`}
+            {visibleResults && `HIDE RESULTS`}
+          </button>
+          {visibleResults && (
+            <>
+              {right.map(result => 
+                <p style={{color: 'green', fontSize: '0.75rem'}} key={result}>
+                  + {he.decode(`${allQuestions[result]}`)}
+                </p>
+              )}
+              {wrong.map(result => 
+                <p style={{color: 'red', fontSize: '0.75rem'}} key={result}>
+                  - {he.decode(`${allQuestions[result]}`)}
+                </p>
+              )}
+            </>
+          )}
+          <button
+            style={buttonStyle}
+            onClick={() => {
+              setPage(0);
+              setScore(0);
+              setAnswers([])
+            }}
+          >
+            Try Again?
+          </button>
+        </>
+      ))}
+      <a style={anchorStyle} href="https://github.com/ijshd7/quizApp" target="_blank" rel="noreferrer">source</a>
     </main>
   )
 }
